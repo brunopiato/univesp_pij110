720a1a743bcbbb1747ce9cd030bc8554398b027d
Bruno Piato <piatobio@gmail.com>
2026-05-13 14:50:09 -0300

    dev: ajustada especialidade na página inicial

--- app/app_home.py
+++ app/app_home.py
@@ -2,7 +2,7 @@
 # @Author: Bruno Piato
 # @Date:   2026-03-31 07:42:14
 # @Last Modified by:   Bruno Piato
-# @Last Modified time: 2026-03-31 16:10:33
+# @Last Modified time: 2026-05-13 14:49:34
 import streamlit as st
 
 
@@ -12,7 +12,8 @@ def st_home():
         st.markdown("### 🏢 Institucional")
         st.caption("Mauricio Henrique Camargo | CNPJ: 11.458.173/0001-74 | Desde 2009")
         st.write("""
-        Especializada em **Suporte Técnico e Manutenção em TI**, nossa empresa combina
-        experiência de mercado com inovação tecnológica. Este sistema de gestão de estoque
-        reflete nosso compromisso com a organização e a excelência operacional.
+        Especializada em **serviços e comercialização de produtos na área elétrica**, 
+        nossa empresa combina experiência de mercado com inovação tecnológica. Este 
+        sistema de gestão de estoque reflete nosso compromisso com a organização e a 
+        excelência operacional.
         """)
